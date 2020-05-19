64bb0458797e4c536fd74dc6c53932cc8cb42b43
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-05-19 15:00:21 +0200

    add npm scss/laravel

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Domande frequenti – Privacy e termini – Google</title>
 
+    <!-- font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"> 
+
     <!-- icon web -->
     <link rel="icon" href="//ssl.gstatic.com/policies/favicon.ico" sizes="32x32">
 </head>
@@ -12,8 +15,6 @@
 
 <!-- PHP -->
 <?php 
-
-
     $google_faq = [
         "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" => [
             'text' => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
